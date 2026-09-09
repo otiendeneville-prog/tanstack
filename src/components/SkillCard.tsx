c05a621ada1e95facaa23c6c29568f2566fad028
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-09 11:34:25 +0300

    add props

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,6 +1,9 @@
+type SkillCardProps={
+  name:string;
+}
 
-
-export default function skillCard() {
+export default function skillCard({name:SkillCardProps}) {
+  const[liked,setLiked] = useState(false);
   return (
     <article>
      <p>Skill: Tanstack Start</p>
