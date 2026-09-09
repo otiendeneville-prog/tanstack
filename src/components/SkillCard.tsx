b4440db8da21b7125a390ee09eb1a2ab04e596b1
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-09 11:53:39 +0300

    import usestate

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+
 type SkillCardProps={
   name:string;
 }
@@ -10,7 +13,9 @@ export default function skillCard({name:SkillCardProps}) {
      <p>Skill</p>
      <h2>{name}</h2>
      <p>{likes} {likes == 1? 'like': 'likes'}</p>
-      <button onClick={()=>setLiked((current)=> !current)}></button>
+      <button onClick={()=>setLiked((current)=> !current)} type="button">
+        <Heart fill={liked ? 'currentColor' :'none'} size={18} />
+      </button>
     </article>
   )
 }
