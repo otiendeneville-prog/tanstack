7706d51fad21996fe5437221651d3b948f9e32a6
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-09 11:09:52 +0300

    cleaning tanstack index file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,9 +6,6 @@ function Home() {
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold">Hello World!</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code>
-      </p>
     </div>
   )
 }
