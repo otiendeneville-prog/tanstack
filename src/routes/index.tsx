96d0bb34142ce6126d3b88afc51f0d29bd36995f
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-09 11:07:45 +0300

    change hello world

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,9 @@ export const Route = createFileRoute('/')({ component: Home })
 function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
+      <h1 className="text-4xl font-bold">Hello World!</h1>
       <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
+        Edit <code>src/routes/index.tsx</code>
       </p>
     </div>
   )
