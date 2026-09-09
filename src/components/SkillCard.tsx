c1dea3789d288da7d2000deccee6017738fbab09
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-09 12:10:13 +0300

    add heart

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -5,7 +5,7 @@ type SkillCardProps={
   name:string;
 }
 
-export default function skillCard({name:SkillCardProps}) {
+export default function skillCard({name}:SkillCardProps) {
   const[liked,setLiked] = useState(false);
   const likes = liked? 1 :0;
   return (
@@ -14,7 +14,6 @@ export default function skillCard({name:SkillCardProps}) {
      <h2>{name}</h2>
      <p>{likes} {likes == 1? 'like': 'likes'}</p>
       <button onClick={()=>setLiked((current)=> !current)} type="button">
-        <Heart fill={liked ? 'currentColor' :'none'} size={18} />
       </button>
     </article>
   )
