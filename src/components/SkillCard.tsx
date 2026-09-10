f7002ef612cac7ba2aeb394150f53024b410092a
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-10 08:35:41 +0300

    add a div

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -10,10 +10,16 @@ export default function skillCard({name}:SkillCardProps) {
   const[liked,setLiked] = useState(false);
   const likes = liked? 1 :0;
   return (
-    <article>
-     <p>Skill</p>
+    <article className="island-shell rise-in rounded-3xl p-5c">
+      <div className="flex items-start justify-between gap-5">
+        <div className="space-y-2">
+            <p>Skill</p>
      <h2>{name}</h2>
      <p>{likes} {likes == 1? 'like': 'likes'}</p>
+        </div>
+
+      </div>
+    
       <button onClick={()=>setLiked((current)=> !current)} type="button">
         <Heart fill={liked ? 'currentColor': 'none'} size={18} />
       </button>
