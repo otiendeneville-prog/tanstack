bddbf44d02678ea20b929b11565efd131b14a04a
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-11 12:33:41 +0300

    add the onClick

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -3,17 +3,20 @@
 import { Heart } from 'lucide-react';
 import {useState} from 'react';
 
-
-const [heart, setHeart] = useState(false)
-
 interface SkillCardProps{
   name:string;
 }
 const SkillCard =({name} : SkillCardProps) =>{
+  const [liked, setLiked] = useState(false);
   return(
     <div className=" p-4 rounded my-2">
       <h1>{name}</h1>
-      <p>{Heart}</p>
+     <Heart
+     className={`cursor-pointer ${liked ? 'fill-red-500 stroke-red-500': 'text-gray-500' }`} 
+       onClick={() =>setLiked(!liked)}
+    
+     
+    />
       
     </div>
   )
