b25019fe6169c2261f0271a59a7c232dc30d3de2
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-11 13:34:27 +0300

    add the the index page

--- src/routes/dashbord/routes.tsx
+++ src/routes/dashbord/routes.tsx
@@ -1,9 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Outlet } from "@tanstack/react-router"
+import { createFileRoute  } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/dashbord/routes')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/dashbord/routes"!</div>
+  return (
+    <main>
+      <aside>
+        <p>sidebar</p>
+      </aside>
+      <aside>
+        <Outlet />
+    </main>
+  )
 }
