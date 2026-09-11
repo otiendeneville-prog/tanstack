7e66c20eaa2c3fcf747b0ec3416cbae835923cd5
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-11 11:58:33 +0300

    add useState from lucid react

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,6 +1,10 @@
-interface SkillCardProps {
-  name: string;
-}
+
+
+import { Heart } from 'lucide-react';
+import {useState} from 'react';
+
+
+const [heart, setHeart] = useState(false)
 
 interface SkillCardProps{
   name:string;
@@ -8,7 +12,9 @@ interface SkillCardProps{
 const SkillCard =({name} : SkillCardProps) =>{
   return(
     <div className=" p-4 rounded my-2">
-      {name}
+      <h1>{name}</h1>
+      <p>{Heart}</p>
+      
     </div>
   )
 }
