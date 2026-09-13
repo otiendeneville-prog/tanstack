0eff9e787b144313941ea32a6c37848fae92bc9c
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-13 12:12:01 +0300

    passing data in the async

--- src/server/pokemon.ts
+++ src/server/pokemon.ts
@@ -13,7 +13,8 @@ export const getPokemonFn = createServerFn({ method:'GET'}).handler(async() =>{
    return data;
 })
 
-export const saveFavoritePokemonFn = createServerFn ({method:'POST'}).handler(async()=>{
+export const saveFavoritePokemonFn = createServerFn ({method:'POST'}).handler(async(data)=>{
     console.log('Saving data to our secure database...')
     await new Promise((resolve) => setTimeout(resolve,1000));
+    return { success:true, saved: data}
 })
