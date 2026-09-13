6911c7fb86f72edaa40a00cfaa3e598fdc3f5fe3
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-13 11:00:53 +0300

    add the pokemon.ts file

--- src/server/pokemon.ts
+++ src/server/pokemon.ts
@@ -1,5 +1,13 @@
 import { createServerFn } from "@tanstack/react-start";
 
 export const getPokemonFn = createServerFn({ method:'GET'}).handler(async() =>{
-
+   const data = await response.json();
+   
+        if(!data.results || data.results.length == 0){
+            throw notFound();
+        }
+       
+   
+        console.log('Loader data:', data)
+        return data;
 })
