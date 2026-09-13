24db61dab36597ef4cf6351c68ab3b0661660698
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-13 11:06:00 +0300

    await poke api

--- src/server/pokemon.ts
+++ src/server/pokemon.ts
@@ -5,13 +5,13 @@ const POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon'
 
 
 export const getPokemonFn = createServerFn({ method:'GET'}).handler(async() =>{
+
+    const response = await fetch(POKE_API_URL);
    const data = await response.json();
-   
+           console.log('Executing a secure database/API call on a server...');
         if(!data.results || data.results.length == 0){
             throw notFound();
         }
-       
    
-        console.log('Executing a secure database/API call on a server...');
         return data;
 })
