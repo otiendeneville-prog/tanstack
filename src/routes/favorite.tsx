ef7cf0a3d2463a1bcacd23288a2cdd15ae0dd49f
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-13 12:24:42 +0300

    add useState

--- src/routes/favorite.tsx
+++ src/routes/favorite.tsx
@@ -1,9 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/favorite')({
   component: FavoritePage,
 })
 
 function FavoritePage() {
+  const[name, setName]= useState('')
+  const[status, setStatus]= useState('')
   return <div>Hello "/favorite"!</div>
 }
