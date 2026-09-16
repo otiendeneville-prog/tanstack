607ccc7cb495ff9c8c4da3352fda29a2a41d4f5a
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-16 19:16:41 +0300

    add the headers

--- src/routes/api/hello.tsx
+++ src/routes/api/hello.tsx
@@ -5,6 +5,13 @@ export const Route = createFileRoute('/api/hello')({
     handlers: {
       GET: async ({request})=>{
         console.log("Someone hit our public API!")
+        return Response.json({message:'Hello, World!'},{
+             headers:{
+          "Cache-Control":"public,s-massage=60",
+          "Access-Control-Allow-Origin":"*"
+        }
+        })
+       
       }
       
     }
