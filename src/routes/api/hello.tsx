daacdc72f4a2f767e18c1f6b40c09a0d64cff2f8
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-16 19:11:34 +0300

    console loging api

--- src/routes/api/hello.tsx
+++ src/routes/api/hello.tsx
@@ -3,6 +3,9 @@ import { createFileRoute } from '@tanstack/react-router'
 export const Route = createFileRoute('/api/hello')({
   server: {
     handlers: {
+      GET: async ({request})=>{
+        console.log("Someone hit our public API!")
+      }
       
     }
   }
